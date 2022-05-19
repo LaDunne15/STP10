94cc76ee488ae1a22ba95bd5f02ef7f6a2c255c6
vladi <vladilsav7772@email.com>
2022-05-19 16:21:19 +0300

    Add project files.

---
+++ ProductApp/Controllers/CoursesController.cs
@@ -0,0 +1,34 @@
+﻿using ProductApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ProductApp.Controllers
+{
+    public class CoursesController : ApiController
+    {
+        static List<Course> courses = Course.CoursesDB();
+
+        [HttpGet]
+        public IHttpActionResult Get()
+        {
+            if (courses.Count == 0)
+                return StatusCode(HttpStatusCode.NoContent);
+
+            return Json(courses);
+        }
+
+        [HttpGet]
+        public IHttpActionResult Get(int id)
+        {
+            Course book = courses.FirstOrDefault(x => x.Id == id);
+            if (book == null)
+                return NotFound();
+
+            return Json(book);
+        }
+    }
+}
---
+++ ProductApp/Models/Course.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProductApp.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public string DateStart { get; set; }
+
+        public static int counter = 0;
+        public Course()
+        {
+            this.Id = ++counter;
+        }
+
+        public static List<Course> CoursesDB()
+        {
+            var res = new List<Course>();
+            res.Add(new Course { Name = "Course 1", Price = 1000, DateStart = "2022, 12, 19" });
+            res.Add(new Course { Name = "Course 2", Price = 900, DateStart = "2022, 11, 1" });
+            res.Add(new Course { Name = "Course 3", Price = 850, DateStart = "2022, 6, 7" });
+            res.Add(new Course { Name = "Course 4", Price = 1100, DateStart = "2022, 8, 23" });
+            res.Add(new Course { Name = "Course 5", Price = 950, DateStart = "2022, 10, 1" });
+
+            return res;
+        }
+    }
+}
